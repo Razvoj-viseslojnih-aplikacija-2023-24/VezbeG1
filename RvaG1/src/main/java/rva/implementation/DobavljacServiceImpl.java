fee9a00724adc3fd971e8be0b0baeeb0ac78a161
VladimirFabri98 <vfficial98@gmail.com>
2024-03-25 16:23:07 +0100

    Testovi za preostale kontrolere

--- RvaG1/src/main/java/rva/implementation/DobavljacServiceImpl.java
+++ RvaG1/src/main/java/rva/implementation/DobavljacServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import rva.models.Artikl;
 import rva.models.Dobavljac;
 import rva.repository.DobavljacRepository;
 import rva.services.DobavljacService;
